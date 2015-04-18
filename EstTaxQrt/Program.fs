26b76852d025874c174f586011c58c0b52dc3ed0
evx001 <eviexeris@gmail.com>
2015-04-18 19:19:25 -0400

    fs works

--- EstTaxQrt/Program.fs
+++ EstTaxQrt/Program.fs
@@ -15,22 +15,26 @@ let sumF1 = (List.sumBy snd f1)
 let sumJ1 = (List.sumBy snd j1) 
 let firstQrtExpSum = (List.sum[sumF1;sumJ1;sumM1])
 // earnings  
-let earnJan = 0 
-let earnFeb = 0   
-let earnMar = 0 
+let earnJan = 0.0 
+let earnFeb = 3300.0   
+let earnMar = 0.0 
 let earn1Qt = (List.sum [earnJan;earnFeb;earnMar]) 
 
+// exp against earn 
+let taxableEarn = earn1Qt - firstQrtExpSum
 // 
-let taxFed = earned4Qt * fedObligat
-let taxAlbany = ( earned4Qt*albanyOblig)
-let taxNYC = ( earned4Qt*nycOglig)
-let taxTotalObligado = sum[taxFed, taxAlbany, taxNYC]   
+let taxFed = taxableEarn * fedObligat
+let taxAlbany = ( taxableEarn*albanyOblig)
+let taxNYC = ( taxableEarn*nycOglig)
+let taxTotalObligado = (List.sum [taxFed; taxAlbany; taxNYC])   
 // output
-printfn "%A"  firstQrtExpSum 
+printfn "taxable Fed $ %A \n "  taxFed
+printfn "taxable NYS $ %A \n "  taxAlbany
+printfn "taxable nyc $ %A \n "  taxNYC
+printfn "taxable Total Obligation $ %A \n "  taxTotalObligado
 
 [<EntryPoint>] 
 let main argv = 
      printfn "%A" argv
      0 // return an integer exit code
-
 
