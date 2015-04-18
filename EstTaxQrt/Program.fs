084e0606d61e7dd68132db1b047ec6b4865b4c0c
evx001 <eviexeris@gmail.com>
2015-04-18 18:38:53 -0400

    fs
    porting from Haskell

--- EstTaxQrt/Program.fs
+++ EstTaxQrt/Program.fs
@@ -1,16 +1,31 @@
 ﻿module Program
 open System
-
+// constants
+let nycOglig        = 0.03534
+let albanyOblig     = 0.059
+let fedObligat      = 0.3 
 // datafiles
 open Data1stQrt
-// unpacked  
 let f1 = Data1stQrt.feb2015
 let j1 = Data1stQrt.jan2015
 let m1 = Data1stQrt.mar2015
+// collecting expenses
 let sumM1 = (List.sumBy snd m1)
 let sumF1 = (List.sumBy snd f1)  
 let sumJ1 = (List.sumBy snd j1) 
-let firstQrtExpSum = (List.sum[sumF1;sumJ1;sumM1])  
+let firstQrtExpSum = (List.sum[sumF1;sumJ1;sumM1])
+// earnings  
+let earnJan = 0 
+let earnFeb = 0   
+let earnMar = 0 
+let earn1Qt = (List.sum [earnJan;earnFeb;earnMar]) 
+
+// 
+let taxFed = earned4Qt * fedObligat
+let taxAlbany = ( earned4Qt*albanyOblig)
+let taxNYC = ( earned4Qt*nycOglig)
+let taxTotalObligado = sum[taxFed, taxAlbany, taxNYC]   
+// output
 printfn "%A"  firstQrtExpSum 
 
 [<EntryPoint>] 
