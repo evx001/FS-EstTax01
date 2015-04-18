3163156ae6970f74830e9e49b5446882b90e928a
evx001 <eviexeris@gmail.com>
2015-04-18 14:21:08 -0400

    source

---
+++ EstTaxQrt/Program.fs
@@ -0,0 +1,21 @@
+﻿module Program
+open System
+
+// datafiles
+open Data1stQrt
+// unpacked  
+let f1 = Data1stQrt.feb2015
+let j1 = Data1stQrt.jan2015
+let m1 = Data1stQrt.mar2015
+let sumM1 = (List.sumBy snd m1)
+let sumF1 = (List.sumBy snd f1)  
+let sumJ1 = (List.sumBy snd j1) 
+let firstQrtExpSum = (List.sum[sumF1;sumJ1;sumM1])  
+printfn "%A"  firstQrtExpSum 
+
+[<EntryPoint>] 
+let main argv = 
+     printfn "%A" argv
+     0 // return an integer exit code
+
+
